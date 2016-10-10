bca8139b644df87ad322098fe01e73d83fd5a77d
armandordorica <armandordorica@gmail.com>
2016-10-10 17:30:57 -0400

    part 1 completed

--- Assignments/Assignment 1/assignment1Main.c
+++ Assignments/Assignment 1/assignment1Main.c
@@ -14,6 +14,7 @@ int getcmd(char *prompt, char *args[], int *background)
   printf("Prompt is: %d\n", prompt); //This prints out a specific long address that never changes regardless 
   //of what the command is. However it changes its value every time you reexecute the code 
 
+
   length = getline(&line, &linecap, stdin);
   printf("length is %d\n", length); 
 
@@ -24,6 +25,7 @@ int getcmd(char *prompt, char *args[], int *background)
   //check is background is specified
 
   if ((loc = index(line, '&')) != NULL){
+    printf("command is to run on the background");
     *background = 1;
     *loc = ' ';
   } else
@@ -37,12 +39,13 @@ int getcmd(char *prompt, char *args[], int *background)
           args[i++] = token;
     }
 
+printf("return value from getcmd is %d\n", i);
     return i;
 }
 
 int main (void)
 {
-  char* args[20];
+  char *args[20];
   int bg;
 
 
@@ -50,22 +53,41 @@ int main (void)
     printf("inside the while loop\n\n");
     bg = 0; 
     int cnt = getcmd("\n>> ", args, &bg); 
+    args[cnt]= NULL; 
     printf("cnt is %d\n", cnt);
     pid_t pid = fork(); 
-    printf("pid %d\n\n", pid);
+    printf("pid of parent is %d\n\n", pid);
 
     if(pid ==0)
     {
+      printf("Inside the child. pid of child is %d\n\n", pid);
       printf("args[0] = %s\n", args[0]);
+      printf("args[1] = %s\n", args[1]);
+      // printf("args[159] = %s\n", args[159]);
 
-       char *args[] = {"./helloExec", "Hello", "World", NULL};
+      //  int i=0; 
+      // for (i=0; i<sizeof(args); i++){
+      //   printf("args[%d] is %d\n", i, args[i]);
+      // }
 
-  execvp(args[0], args);
-    // execvp(args[0],args); 
+       // char *args[] = {"./helloExec", "Hello", "World", NULL};
+      // char *args[] = {"./helloExec", NULL};
+
+       printf("args[0] = %s\n", args[0]);
+
+        if ((execvp(args[0], args)) == -1 ){
+
+          printf("execvp is not working\n");
+        } //something WRONG WITH THIS LINE 
+
+    printf("Right before execvp\n");
+    execvp(args[0],args); 
     printf("after execvp\n");
     exit(1); 
     }else{
       int status; 
+      printf("Status is %d\n", status); 
+      printf("Waiting for child to complete execution.\n");
       waitpid(pid, &status, 0); //patent waits for child to complete its execution
 
     /*the steps can be ...: 
