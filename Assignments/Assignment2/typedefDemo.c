93612e1eeb8c7acafa951ed860fc37faa781c3e5
armandordorica <armandordorica@gmail.com>
2016-11-04 11:21:52 -0400

    typedef demo

---
+++ Assignments/Assignment2/typedefDemo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//typef is a keyword to give a name for another typef
+
+typedef int INT32; 
+typedef char MYCHR; 
+
+typedef struct mystruct_t {
+    int a; 
+    int b; 
+ } MYSTRX; 
+
+int main (int argc, char *arv[])
+{
+    INT32 i; 
+    MYCHR mystr[20] = "Hello World"; 
+
+    MYSTRX gold; 
+
+    gold.a = 2; 
+    gold.b  = 4;
+
+    printf ("i = %d\n", i); 
+    printf("mystr = %s\n", mystr);
+
+    printf("gold.a = %d\n", gold.a); 
+    printf("gold.b = %d\n", gold.b);
+
+    return 0; 
+
+}
